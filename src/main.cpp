a44e005d08c6ce450f35dbd1c5db2a48c013dcd4
Colton Broughton <cole.broughton@gmail.com>
2021-07-12 11:17:22 -0700

    added looping over adc pins

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@
 #include "selectMuxPin.h"
 #include "collectSamples.h"
 #include "avgVal.h"
-#include "appendLineToFile.h"
 
 using std::vector;
 
@@ -21,46 +20,45 @@ unsigned long mux1Intrvl [8] { 20000,  90000,  90000,  90000,  20000,  20000, 27
 unsigned long outTime=0;
 
 const unsigned long printIntrvl=10000; // output sensor data every 10 seconds.
-
-adsGain_t mux1Gain [8] {GAIN_TWOTHIRDS, GAIN_SIXTEEN,   GAIN_FOUR, GAIN_TWOTHIRDS,\
-                        GAIN_ONE,       GAIN_TWOTHIRDS, GAIN_ONE,  GAIN_TWO}; // gain array for adjusting the ADC to be within a proper range for each sensor value. this is adc pin 0 or mux 1
-Adafruit_ADS1115 ads;	// Construct an ads1115 instance to read single ended values
 vector<float> mux1Vals = {0,0,0,0,0,0,0,0};
+
+Adafruit_ADS1115 ads;	// Construct an ads1115 instance to read single ended values
 
 void setup() 
 {   
     Serial.begin(9600); // serial output to print out the sensor values
-    delay(5000);        // included only to allow vscode + platformio to catch the first Serial print calls.
-    Serial.println("Serial init complete");
     ads.begin();  // Initialize ads1115 at the default address 0x48
+    ads.setGain(GAIN_ONE);
     for (int i=0; i < 3; i++) 
     {
         pinMode(mux1S[i], OUTPUT);
     }
 }
-
 void loop() {
-    for (int muxPin=0; muxPin<8; muxPin++) 
+    for (int pin : adcPins)  // cycling through the adc pins (0-3)
     {
-        if (millis() - mux1Times[muxPin] > mux1Intrvl[muxPin] / timeMod)  // check if the last time the pin was polled is greater than the sampling interval
+        for (int muxPin=0; muxPin<8; muxPin++) // cycling through the mux pins (0-7)
         {
-            selectMuxPin(muxPin, mux1S);  // switches the mux to the pin we're interested in
-            ads.setGain(mux1Gain[muxPin]);
-            vector<float> sensValsMux1 = collectSamples(adcPins[0], ads);  // returns a vector of 20 sensor readings over 1 second. 
-            float sensValMux1 = avgVal(sensValsMux1);
-            mux1Vals[muxPin] = sensValMux1;
-            mux1Times[muxPin] = millis();  // storing the pin access time to be checked next loop
-        }
-        if (millis() - (outTime) > printIntrvl / timeMod) // I only want to print the output (ie send to file) every 10/timeMod seconds!
-        { 
-            String outString = "";
-            for (int i : mux1Vals)
+            if (millis() - mux1Times[muxPin] > mux1Intrvl[muxPin] / timeMod)  // check if the last time the pin was polled is greater than the sampling interval
             {
-                float mux1Volt = ads.computeVolts(i);
-                outString += (String)mux1Volt + ",";
+                selectMuxPin(muxPin, mux1S);  // switches the mux to the pin we're interested in
+                vector<float> sensValsMux1 = collectSamples(pin, ads);  // returns a vector of 20 sensor readings over 1 second. 
+                float sensValMux1 = avgVal(sensValsMux1);
+                mux1Vals[muxPin] = sensValMux1;
+                mux1Times[muxPin] = millis();  // storing the pin access time to be checked next loop
             }
-            Serial.println(outString);
-            outTime = millis();
-        }
-    }   
+            if (millis() - (outTime) > printIntrvl / timeMod) // I only want to print the output (ie send to file) every 10/timeMod seconds!
+            { 
+                String outString = "";
+                for (int i : mux1Vals)
+                {
+                    float mux1Volt = ads.computeVolts(i);
+                    outString += (String)mux1Volt + ",";
+                }
+                Serial.println(outString);
+                outTime = millis();
+            }
+        }  
+        Serial.println(",,,,,,,"); // blank line indicating switching muxes.
+    }
 }
