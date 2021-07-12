1b77ebed596e5e6372c0297e48929b4f5efa01ea
Colton Broughton <49500055+CoBr8@users.noreply.github.com>
2021-07-12 09:52:42 -0700

    Delete appendLineToFile.cpp
    not implemented

--- src/appendLineToFile.cpp
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-#include <fstream>
-void appendLineToFile(std::string filepath, String line)
-{   
-    using namespace std;
-    ofstream file;
-    file.open(filepath, ios::out | ios::app);
-    file << line << std::endl;
-}
